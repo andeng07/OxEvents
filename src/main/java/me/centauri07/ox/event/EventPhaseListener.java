f318ecd582d04b49ef5a401545989183920a0cc9
andeng07 <sumilangandreijohn@gmail.com>
2025-06-23 02:25:07 +0800

    refactor: remove unnecessary properties

--- src/main/java/me/centauri07/ox/event/EventPhaseListener.java
+++ src/main/java/me/centauri07/ox/event/EventPhaseListener.java
@@ -1,18 +1,8 @@
 package me.centauri07.ox.event;
 
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class EventPhaseListener implements Listener {
 
-    protected final JavaPlugin plugin;
-    protected final OxEvent oxEvent;
-    protected final EventPhase phase;
-
-    protected EventPhaseListener(JavaPlugin plugin, OxEvent oxEvent, EventPhase phase) {
-        this.plugin = plugin;
-        this.oxEvent = oxEvent;
-        this.phase = phase;
-    }
 
 }
